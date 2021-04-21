6a372a7cd492476d35948a0b0d1d3193baa2bbb7
Romain de Laage <romain.delaage@rdelaage.ovh>
2021-04-21 10:57:21 +0200

    Add header file for function

---
+++ Fonction_Regle/Fonction.h
@@ -0,0 +1,50 @@
+#ifndef _FONCTION_H
+#define _FONCTION_H
+
+class Regle {
+	private:
+		int seuils[8];
+		Etat destination;
+
+	public:
+		const Etat& getDestination() const { return destination; }
+		bool verify(const Voisinage& voisins) const;
+};
+
+class RegleAvecEtatCourant: public Regle {
+	private:
+		Etat etatCourant;
+
+	public:
+		bool verify(const Voisinage& voisins, const Cellule& cellule) const;
+};
+
+class Fonction {
+	private:
+		Etat etatDefaut;
+		Regle** regles;
+
+	public:
+		Fonction();
+		~Fonction();
+		const Etat& getEtatDefaut() const { return etatDefaut; }
+		void setEtatDefaut(const Etat& nouveau) { etatDefaut = nouveau; }
+		void ajouterRegle(const Etat& destination, const int seuils[8]);
+		const Etat& getEtatSuivant(const Voisinage& voisins) const;
+};
+
+class FonctionAvecEtatCourant {
+	private:
+		Etat etatDefaut;
+		RegleAvecEtatCourant** regles;
+	
+	public:
+		FonctionAvecEtatCourant();
+		~FonctionAvecEtatCourant();
+		const Etat& getEtatDefaut() const { return etatDefaut; }
+		void setEtatDefaut(const Etat& nouveau) { etatDefaut = nouveau; }
+		void ajouterRegle(const Etat& destination, const int seuils[8], const Etat& courant);
+		const Etat& getEtatSuivant(const Voisinage& voisins, const Cellule& cellule) const;
+};
+
+#endif
