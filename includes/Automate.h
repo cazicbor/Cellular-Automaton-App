c99e61001bd8924351ca461a0018f5d2c88c6cb0
Romain de Laage <romain.delaage@rdelaage.ovh>
2021-06-02 21:15:45 +0200

    Add Doxygen doc generation

--- includes/Automate.h
+++ includes/Automate.h
@@ -5,6 +5,9 @@
 #include <Fonction.h>
 #include <voisinage.h>
 
+/// La classe automate se charge de gérer les informations d'un automate (fonction de transition, règle de voisinage, réseau, ...) et joue également le rôle de simulateur
+///
+/// Cette classe est un singletion est l'objet unique n'est disponible qu'avec la méthode statique getInstance(). Ainsi, il est impossible de créer des objets de cette classe. La classe agrège une fonction et une règle de voisinage dont l'utilisateur a la responsabilité. Il est également possible de laisser l'automate simuler avec un délai réglable.
 class Automate {
 	private:
 		static std::unique_ptr<Automate> instance;
