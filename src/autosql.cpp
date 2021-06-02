a832180bbd6362606153cf7dd6492d4e79212b4a
Romain de Laage <romain.delaage@rdelaage.ovh>
2021-06-02 20:52:36 +0200

    Add tables to save models

--- src/autosql.cpp
+++ src/autosql.cpp
@@ -7,7 +7,10 @@ Database::Database(std::string path): db(QSqlDatabase::addDatabase("QSQLITE")) {
 		throw "Unable to open database";
 	}
 
-	db.exec("CREATE TABLE IF NOT EXISTS automates (nom VARCHAR(30) PRIMARY KEY, voisinage INTEGER, fonction INTEGER)");
+	db.exec("CREATE TABLE IF NOT EXISTS automates (nom VARCHAR(30) PRIMARY KEY, defaut INTEGER)");
+	db.exec("CREATE TABLE IF NOT EXISTS regles_transition (id VARCHAR(30) REFERENCES automate(nom), min1 INTEGER, min2 INTEGER, min3 INTEGER, min4 INTEGER, min5 INTEGER, min6 INTEGER, min7 INTEGER, min8 INTEGER, max1 INTEGER, max2 INTEGER, max3 INTEGER, max4 INTEGER, max5 INTEGER, max6 INTEGER, max7 INTEGER, max8 INTEGER, courant INTEGER, destination INTEGER)");
+	db.exec("CREATE TABLE IF NOT EXISTS regles_voisinage (id VARCHAR(30) PRIMARY KEY REFERENCES automates(nom), type INTEGER)");
+	db.exec("CREATE TABLE IF NOT EXISTS coord_voisinage (id VARCHAR(30) REFERENCES automates(nom), x INTEGER, y INTEGER)");
 }
 
 std::vector<QString> Database::getAutomates() const {
