c8b8ef63acb1c346d426fb4eed4c011d9f5de49a
Luning Yang <luning.yang@etu.utc.fr>
2021-06-07 18:49:12 +0000

    définir la frame paramétrage de modèle

--- includes/parametragemodele.h
+++ includes/parametragemodele.h
@@ -8,6 +8,7 @@
 #include <QPushButton>
 #include <QWidget>
 #include <QLineEdit>
+#include <QSpinBox>
 #include <QLabel>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
@@ -27,11 +28,20 @@ class NouveauModele : public QWidget {
     Q_OBJECT
     QGridLayout* general;
 
-    //définition de la frame
+    //définition de la frame paramétrage du modèle
+    QWidget* fenetre_init;
+    QGridLayout* form_init;
+    QFormLayout* form_param;
+
+    QSpinBox* nb_etats;
+    QComboBox* liste_voisinage;
+    QComboBox* liste_regle_transition;
+    QPushButton* bouton_valide;
+
 
     //définition de la frame contenant la grille de sélection du voisinage
     QLabel* label_init;
-    QWidget* fenetre_init;
+
     QWidget* fenetre_grid;
     QTableWidget* grid;
 public:
