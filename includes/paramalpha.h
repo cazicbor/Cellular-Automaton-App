4362ab2369ca3d0f5242befc3b083a6850928b35
Romain de Laage <romain.delaage@rdelaage.ovh>
2021-06-10 13:58:46 +0200

    Fix to compile

--- includes/paramalpha.h
+++ includes/paramalpha.h
@@ -1,7 +1,17 @@
 #ifndef _PARAMALPHA_H
 #define _PARAMALPHA_H
 
-class ParamAlpha: public QObject {
+#include <QObject>
+#include <QPushButton>
+#include <QSpinBox>
+#include <QLineEdit>
+#include <QGridLayout>
+#include <QWidget>
+#include <QFormLayout>
+#include <reseau_cellule_etats.h>
+#include <Automate.h>
+
+class ParamAlpha: public QWidget {
 	Q_OBJECT
 	private:
 		EnsembleEtat ense;
@@ -14,7 +24,7 @@ class ParamAlpha: public QObject {
 		QSpinBox* green[8];
 		QSpinBox* blue[8];
 
-		QPushButton valider("Valider", this);
+		QPushButton* valider;
 	public slots:
 		void addEtats(const int nbEtats);
 		void valide();
